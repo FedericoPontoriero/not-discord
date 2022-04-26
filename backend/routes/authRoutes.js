46afe4e4cc9d13bbf8ec02cc2f8ac5447fa32ddb
Federico <federicolucaspontoriero@gmail.com>
2022-04-26 17:18:02 -0300

    Added validator

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const authControllers = require('../controllers/auth/authControllers');
+const Joi = require('joi');
+const validator = require('express-joi-validation').createValidator({});
 
-router.post('/register', (req, res) => {
-	res.send('register route');
+// Validation Schemas
+const registerSchema = Joi.object({
+	username: Joi.string().min(3).max(12),
+	password: Joi.string().min(6).max(12),
+	mail: Joi.string().email(),
+});
+const loginSchema = Joi.object({
+	password: Joi.string().min(6).max(12),
+	mail: Joi.string().email(),
 });
 
-router.post('/login', (req, res) => {
-	res.send('login route');
-});
+// Post Routes
+router.post(
+	'/register',
+	validator.body(registerSchema),
+	authControllers.controllers.postRegister
+);
+router.post(
+	'/login',
+	validator.body(loginSchema),
+	authControllers.controllers.postLogin
+);
 
 module.exports = router;
