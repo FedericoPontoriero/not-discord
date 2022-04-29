45515dc1a0de267b4cc945d95ba3c275e09d2948
Federico <federicolucaspontoriero@gmail.com>
2022-04-29 17:02:17 -0300

    validation for login

---
+++ frontend/src/authPages/LoginPage/LoginPageInputs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import InputWithLabel from '../../shared/components/inputWithLabel';
+
+function LoginPageInputs({ mail, setMail, password, setPassword }) {
+	return (
+		<>
+			<InputWithLabel
+				value={mail}
+				setValue={setMail}
+				label='E-mail'
+				type='text'
+				placeholder='Enter e-mail adress'
+			/>
+
+			<InputWithLabel
+				value={password}
+				setValue={setPassword}
+				label='password'
+				type='password'
+				placeholder='Enter Password'
+			/>
+		</>
+	);
+}
+
+export default LoginPageInputs;
---
+++ frontend/src/store/store.js
@@ -0,0 +1,16 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { combineReducers, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from './reducers/authReducer';
+
+const rootReducer = combineReducers({
+	auth: authReducer,
+});
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
