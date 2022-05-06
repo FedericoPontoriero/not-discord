581427fb439326649f800c40820c8089dea49cdf
Federico <federicolucaspontoriero@gmail.com>
2022-05-06 02:08:31 -0300

    invitations

--- frontend/src/authPages/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
+++ frontend/src/authPages/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { styled } from '@mui/system';
 
+const DUMMY_INVITATIONS = [
+	{
+		_id: '1',
+		senderId: {
+			username: 'Mark',
+			mail: 'dummy@ad.com',
+		},
+	},
+	{
+		_id: '2',
+		senderId: {
+			username: 'John',
+			mail: 'John@ad.com',
+		},
+	},
+];
+
 const MainContainer = styled('div')({
 	width: '100%',
 	height: '22%',
@@ -11,7 +28,18 @@ const MainContainer = styled('div')({
 });
 
 function PendingInvitationsList() {
-	return <MainContainer></MainContainer>;
+	return (
+		<MainContainer>
+			{DUMMY_INVITATIONS.map(invitation => (
+				<PendingInvitationsListItem
+					key={invitation._id}
+					id={invitation._id}
+					username={invitation.senderId.username}
+					mail={invitation.senderId.mail}
+				/>
+			))}
+		</MainContainer>
+	);
 }
 
 export default PendingInvitationsList;
---
+++ frontend/src/authPages/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsListItem.js
@@ -0,0 +1,54 @@
+import { Tooltip, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useState } from 'react';
+import avatar from '../../../../shared/components/avatar';
+
+function PendingInvitationsListItem({
+	id,
+	username,
+	mail,
+	acceptFriendInvitation = () => {},
+	rejectFriendInvitation = () => {},
+}) {
+	const [buttonsDisabled, setButtonsDisabled] = useState(false);
+
+	const handleAcceptInvitation = () => {
+		acceptFriendInvitation({ id });
+		setButtonsDisabled(true);
+	};
+
+	const handleRejectInvitation = () => {
+		rejectFriendInvitation({ id });
+		setButtonsDisabled(true);
+	};
+
+	return (
+		<Tooltip title={mail}>
+			<div style={{ width: '100%' }}>
+				<Box
+					sx={{
+						width: '100%',
+						height: '42px',
+						marginTop: '10px',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+					}}>
+					<avatar username={username} />
+					<Typography
+						sx={{
+							marginLeft: '7px',
+							fontWeight: 700,
+							color: '#8e9297',
+							flexGrow: 1,
+						}}
+						variant='subtitle1'>
+						{username}
+					</Typography>
+				</Box>
+			</div>
+		</Tooltip>
+	);
+}
+
+export default PendingInvitationsListItem;
